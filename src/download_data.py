93937df7d55bdb9d5605ee2fc178c0207c587eff
Spencer <smines1@binghamton.edu>
2025-03-15 10:00:08 -0400

    requirements

--- src/download_data.py
+++ src/download_data.py
@@ -1,16 +1,30 @@
 import requests
+from tqdm import tqdm
+import os
 
-def download_nasa(output_file = 'nasa_exoplanet_data.csv'):
+def download_nasa(output_file = 'src/nasa_exoplanet_data.csv'):
     url = "https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+*+from+pscomppars&format=csv"
 
-    response = requests.get(url)
+    print(f"Downloading data from {url} to {output_file}")
+    response = requests.get(url, stream=True)
 
     if response.status_code == 200:
+        # Get file size if available
+        total_size = int(response.headers.get('content-length', 0))
         with open(output_file, 'wb') as file:
-            file.write(response.content)
+            with tqdm(total=total_size, unit='B', unit_scale=True, desc=output_file) as pbar:
+                for chunk in response.iter_content(chunk_size=1024):
+                    if chunk:  # filter out keep-alive chunks
+                        file.write(chunk)
+                        pbar.update(len(chunk))
         print(f"Data successfully downloaded to {output_file}")
     else:
         print(f"Failed to download data. Status code: {response.status_code}")
 
 if __name__ == "__main__":
+    # if not in root directory, throw an error
+    if os.path.dirname(__file__) != os.path.abspath('src'):
+        raise ValueError("This script must be run from the root directory, not src/")
+
+    print("Downloading NASA exoplanet data...")
     download_nasa()
